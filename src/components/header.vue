434b5e33f4cade588939d0f2035e20ce7dfeb72f
jackiecookie <2711002357@qq.com>
2017-08-29 16:31:55 +0800

    购物车

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,21 @@
 <template>
+  <section>
     <header id="header_top">
       <icon name="back" :scale="7" v-if="goBack" @click="$router.go(-1)"></icon>
       <span class="head_title">
         {{title}}
       </span>
     </header>
+    <cartIcon></cartIcon>
+  </section>
+
 </template>
 <script>
+  import cartIcon from '../pages/cart/cart'
   export default {
+    components: {
+      'cartIcon': cartIcon
+    },
     props: ['title', 'goBack']
   }
 </script>
@@ -20,9 +28,10 @@
     z-index: 100;
     left: 0;
     top: 0;
-    @include wh(100%, 1.95rem);
+    @include wh(100%, 2.5rem);
     .head_title{
       left: 40%;
+      top: 20%;
       position: absolute;
     }
   }
---
+++ src/pages/cart/cart.vue
@@ -0,0 +1,58 @@
+<template>
+ <section class="buy_cart_container">
+   <div class="cart_icon_container">
+     <span v-if="cartCount" class="cart_list_length">
+       {{cartCount}}
+     </span>
+     <icon name="basket" scale="3" ></icon>
+   </div>
+ </section>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        cartCount: 0
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  @import '../../style/mixin';
+  .buy_cart_container{
+    position: fixed;
+    top: .7rem;
+    left: 0;
+    z-index: 113;
+    display: flex;
+    width: 100%;
+    height:1.95rem;
+    .cart_list_length{
+      position: absolute;
+      top: -.2em;
+      right: -.25rem;
+      background-color: #ff461d;
+      line-height: .6rem;
+      text-align: center;
+      border-radius: 50%;
+      border: .025rem solid #ff461d;
+      min-width: .7rem;
+      height: .7rem;
+      font-size: .5rem;
+      color: #fff;
+      font-family: Helvetica Neue,Tahoma,Arial;
+    }
+    .cart_icon_container{
+      display: flex;
+      position: absolute;
+      padding: .2rem;
+      border: .18rem solid #444;
+      border-radius: 50%;
+      right: .5rem;
+      top: -.7rem;
+    }
+  }
+
+</style>
